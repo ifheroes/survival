dc997ce5750d97ebc8ba4a57b20ac3f4647561c0
Akabe <Office@AkabeX86.com>
2024-04-27 18:54:30 +0200

    Update CommandWorld.java
    Worldhandler Geupdated (Testphase)

--- src/main/java/com/akabex86/commands/CommandWorld.java
+++ src/main/java/com/akabex86/commands/CommandWorld.java
@@ -1,8 +1,11 @@
 package com.akabex86.commands;
 
 import com.akabex86.main.Main;
+import com.akabex86.utils.WorldHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -40,6 +43,10 @@ public class CommandWorld implements CommandExecutor {
                 }
                 p.sendMessage("§eWelten§7:§8"+worldlist.toString());
                 return true;
+            }
+            if(args[0].equalsIgnoreCase("test")){
+                //Creates a testworld with the seed "0"
+                WorldHandler.createWorld("testworld", World.Environment.NORMAL, WorldType.FLAT, (long)0);
             }
             //returning true returns the usage.
             return true;
