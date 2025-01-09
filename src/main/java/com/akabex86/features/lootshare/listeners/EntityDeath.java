ece744a7e208a0b481cdef7d3dc81faed421740f
I_Dev <devdiamant@gmail.com>
2025-01-09 21:59:04 +0100

    Added Highest Damage Support for Boss Loot

--- src/main/java/com/akabex86/features/lootshare/listeners/EntityDeath.java
+++ src/main/java/com/akabex86/features/lootshare/listeners/EntityDeath.java
@@ -15,13 +15,15 @@ import com.akabex86.features.lootshare.LootShare;
 
 public class EntityDeath implements Listener{
 
+
+	private static Comparator<Map.Entry<Player, Double>> comparator = Map.Entry.<Player, Double>comparingByValue().reversed();
+	
 	@EventHandler
 	public void entityDeath(EntityDeathEvent event) {
 		LivingEntity killedEntity = event.getEntity();
 		if(!LootShare.isValidEntity(killedEntity)) return;
 		if(killedEntity.getKiller() == null) return;
 		
-		Comparator<Map.Entry<Player, Double>> comparator = Map.Entry.<Player, Double>comparingByValue().reversed();
 		LinkedHashMap<Player, Double> damageComposit = LootShare.getEntityDamageRegistry().getDamageComposite(killedEntity, comparator);
 		
 		double maxDamage = damageComposit.values().stream().findFirst().orElse(0D); if(maxDamage == 0D) return;
@@ -45,12 +47,16 @@ public class EntityDeath implements Listener{
 		if(!(killedEntity instanceof Boss)) return;
 		if(killedEntity.getKiller() == null) return;
 		
+		LinkedHashMap<Player, Double> damageComposit = LootShare.getEntityDamageRegistry().getDamageComposite(killedEntity, comparator);
+		Player player = damageComposit.keySet().stream().findFirst().orElse(killedEntity.getKiller());
+		
 		event.getDrops().forEach(item -> {
-			LootShare.spawnLoot(item, killedEntity.getLocation(), killedEntity.getKiller());
-			LootShare.spawnXP(killedEntity.getKiller().getLocation(), event.getDroppedExp());
+			LootShare.spawnLoot(item, killedEntity.getLocation(), player);
+			LootShare.spawnXP(player.getLocation(), event.getDroppedExp());
 		});
 		
 		event.setDroppedExp(0);
 		event.getDrops().clear();
 	}
+	
 }
