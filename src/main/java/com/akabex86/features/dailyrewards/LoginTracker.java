ac361572391b5026fb3b95c2c13510709f3212db
I_Dev <devdiamant@gmail.com>
2025-05-15 22:20:36 +0200

    Introducing DailyRewardMVP - Updates to POM and Plugin.yml

---
+++ src/main/java/com/akabex86/features/dailyrewards/LoginTracker.java
@@ -0,0 +1,43 @@
+package com.akabex86.features.dailyrewards;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.akabex86.main.Main;
+
+import de.ifheroes.core.InfinityHeroesCore;
+import de.ifheroes.core.InfinityHeroesCoreAPI;
+import de.ifheroes.core.profile.HeroProfile;
+import de.ifheroes.core.profile.levelstructur.DomainKey;
+
+public class LoginTracker {
+
+	private static InfinityHeroesCoreAPI coreAPI = InfinityHeroesCore.getAPI();
+	private static DomainKey timeKey = new DomainKey(JavaPlugin.getPlugin(Main.class), "lastlogin");
+	
+	private LoginTracker(){}
+	
+	public static void setLastLogin(OfflinePlayer player, long time) {
+		coreAPI.getProfile((Player) player).ifPresent(profile -> profile.getPluginData().set(timeKey, time));
+	}
+	
+	public static long getLastLoginMilis(OfflinePlayer player) {
+		Optional<HeroProfile> profile = coreAPI.getProfile((Player) player);
+		if(profile.isEmpty()) return System.currentTimeMillis(); //Might need to change his result to 0
+		
+		Optional<Long> time = profile.get().getPluginData().get(timeKey, Long.class);
+		if(time.isPresent()) return time.get();
+		return System.currentTimeMillis(); //Might need to change his result to 0
+	}
+	
+	public static Calendar getLastLogin(OfflinePlayer player) {
+		Calendar calender = Calendar.getInstance();
+		calender.setTimeInMillis(getLastLoginMilis(player));
+		return calender;
+	}
+	
+}
--- src/main/java/com/akabex86/main/Main.java
+++ src/main/java/com/akabex86/main/Main.java
@@ -4,6 +4,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.akabex86.commands.CommandBook;
@@ -28,6 +32,8 @@ import com.akabex86.commands.CommandWarp;
 import com.akabex86.commands.CommandWarps;
 import com.akabex86.commands.CommandZone;
 import com.akabex86.features.FeatureManager;
+import com.akabex86.features.dailyrewards.LoginTracker;
+import com.akabex86.features.dailyrewards.commands.CheckTrack;
 import com.akabex86.listeners.PacketListeners;
 import com.akabex86.listeners._EventLoader;
 import com.akabex86.utils.Config;
@@ -77,6 +83,15 @@ public class Main extends JavaPlugin {
         featureManager = new FeatureManager(featuresPackage);
         featureManager.initializeFeatures();
         logger.log(Level.INFO, "All Features loaded!");
+        
+        getCommand("checkLastLogin").setExecutor(new CommandExecutor() {
+			
+			@Override
+			public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+				sender.sendMessage(LoginTracker.getLastLoginMilis((Player) sender)+"");
+				return true;
+			}
+		});
         
         logger.log(Level.INFO,"Survival System aktiviert!");
     }
@@ -173,8 +188,10 @@ public class Main extends JavaPlugin {
         CommandBroadcast bc= new CommandBroadcast(this);
         getCommand("broadcast").setExecutor(bc);
         getCommand("broadcast").setTabCompleter(bc);
+       
 
     }
+    
     private void loadConfig(){
         Config.createMainFolder();
         Config.createFolder("kits");
